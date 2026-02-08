103cbed0fd247c6c91e348f2fb7ef0705f97697c
Hos9 <shahin9315@gmail.com>
2026-02-08 14:50:57 +0600

    Problem 4 done

--- problem-4.js
+++ problem-4.js
@@ -7,25 +7,35 @@ function gonoVote(array) {
   let ha = 0;
   let na = 0;
 
-  for (const vote of array) {
+  for (let vote of array) {
     if (typeof vote !== "string") {
       return "Invalid";
-    } else {
-      vote.trim().toLowerCase();
     }
 
+    vote = vote.trim().toLowerCase();
+
     if (vote === "ha") {
-      return ha++;
+      ha++;
     } else if (vote === "na") {
-      return na++;
-    } else return "Invalid";
+      na++;
+    } else {
+      return "Invalid";
+    }
   }
 
   if (ha > na) {
     return true;
-  } else if ((ha = na)) {
+  } else if (ha === na) {
     return "equal";
-  } else return false;
+  } else {
+    return false;
+  }
 }
 
 console.log(gonoVote(["ha", "na", "ha", "na"]));
+console.log(gonoVote(["Ma", "na", "ha", "na"]));
+console.log(gonoVote(["Ha", "Na", " ha ", "na"]));
+console.log(gonoVote(["ha", "na", "na"]));
+console.log(gonoVote(["ha", "ha", "ha", "na"]));
+console.log(gonoVote("ha, na"));
+console.log(gonoVote(12345));
